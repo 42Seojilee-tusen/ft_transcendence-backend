ccd8179ee258f05b7ca5e23322a01a30f25e9d8b
JeongTJ <dmswl022329@gmail.com>
2025-01-25 13:04:59 +0900

    fix: 하드코딩 되어있던 Oauth API키를 .env로 이동

--- srcs/backend/srcs/mysite/api/views.py
+++ srcs/backend/srcs/mysite/api/views.py
@@ -6,6 +6,8 @@ from api.models import TCDUser
 from rest_framework_simplejwt.tokens import AccessToken, RefreshToken
 from rest_framework_simplejwt.exceptions import TokenError
 import logging
+import os
+from dotenv import load_dotenv
 
 logger = logging.getLogger('api') 
 # Create your views here.
@@ -44,9 +46,9 @@ def index(request):
 
 # 로그인 페이지
 def login(request):
-    auth_url = "https://api.intra.42.fr/oauth/authorize"
-    client_id = "u-s4t2ud-c380f46281110b1a4bacedf94922414a41c33a04f9ea0718674e7ea66232fb26"
-    redirect_uri = "http://localhost:8000/api/auth/callback"
+    auth_url = os.getenv("AUTH_URL")
+    client_id = os.getenv("UID_KEY")
+    redirect_uri = os.getenv("REDIRECTION_URI")
     response_type = "code"
 
     auth_url = f"{auth_url}?client_id={client_id}&redirect_uri={redirect_uri}&response_type={response_type}"
@@ -64,9 +66,9 @@ def login_callback(request):
 
     data = {
         "code": code,
-        "client_id": "u-s4t2ud-c380f46281110b1a4bacedf94922414a41c33a04f9ea0718674e7ea66232fb26",
-        "client_secret": "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",
-        "redirect_uri": "http://localhost:8000/api/auth/callback",
+        "client_id": os.getenv("UID_KEY"),
+        "client_secret": os.getenv("SECRET_KEY"),
+        "redirect_uri": os.getenv("REDIRECTION_URI"),
         "grant_type": "authorization_code",
     }
     response = requests.post(token_url, data=data)
@@ -74,13 +76,9 @@ def login_callback(request):
 
     if "error" in token_response:
         return JsonResponse({"error": token_response["error"]}, status=400)
-    
-    access_token = token_response["access_token"]
-    expires_in = token_response["expires_in"]
-    refresh_token = token_response["refresh_token"]
 
     api_url = "https://api.intra.42.fr/v2/me"
-    headers = {"Authorization": f"Bearer {access_token}"}
+    headers = {"Authorization": f"Bearer {token_response["access_token"]}"}
     response = requests.get(api_url, headers=headers).json()
 
     user, created = TCDUser.objects.update_or_create(
@@ -92,13 +90,16 @@ def login_callback(request):
             "image_url": response["image"]["link"],
         },
     )
-    
+
     if created:
         logging.info(f"새로운 유저가 생성되었습니다: {user}")
     else:
         logging.info(f"기존 유저가 업데이트되었습니다: {user}")
+
     refresh = RefreshToken.for_user(user)
-    
+
+    # 이부분은 프론트엔트 테스트 코드가 있어야 수정 가능함.
+    # AT, RT를 쿠키에 저장하여 관리할지 정해야하는 부분
     response = JsonResponse({
         "message": "Login successful",
         "access_token": str(refresh.access_token),
