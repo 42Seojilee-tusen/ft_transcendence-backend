15dd43e5255f0c3aa4b5be23b159c7b7bf05922e
seojilee <seojilee@student.42seoul.kr>
2025-02-10 22:51:48 +0900

    chore: remove test code

--- srcs/backend/srcs/mysite/users/models.py
+++ srcs/backend/srcs/mysite/users/models.py
@@ -5,6 +5,3 @@ class CustomUser(AbstractUser):
     id = models.IntegerField(primary_key=True, blank=True,help_text="유저 고유 id")
     email = models.CharField(max_length=100, blank=True, help_text="인트라 이메일")
     profile_image = models.FileField(upload_to='profiles/', null=True, help_text="프로필 이미지 경로")
-
-    def __eq__(self, other):
-        return True if self.id == other.id else False
