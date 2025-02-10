1932104ac69a57e66c98f321b6492a541d1d7c2a
seojilee <seojilee@student.42seoul.kr>
2025-02-10 21:24:18 +0900

    feat: list up migration history

--- srcs/backend/srcs/mysite/game_records/serializers.py
+++ srcs/backend/srcs/mysite/game_records/serializers.py
@@ -1,8 +1,48 @@
 # serializers.py
 from rest_framework import serializers
-from .models import OneOnOneMatch
+from rest_framework.response import Response
+from .models import OneOnOneMatch, UserOneOnOneGameRecord, TournamentMatch, UserTournamentGameRecord
+from users.models import CustomUser
 
+# {
+#    "total_match_history": [
+#	      { #		        "match_type": "배틀", #		        "total_match": 100,
+#		        "win": 50,
+#		        "lose": 50,
+#	      }
+#	      {
+#		        "match_type": "토너먼트",
+#		        "total_match": 100,
+#		        "1st": 25,
+#		        "2nd": 25,
+#		        "3rd": 25,
+#		        "4th": 25,
+#	      }
+#    ],
+#    "match_history": [
+#		    {
+#		  	    "date": [2025, 2, 7],
+#		        "match_type": "배틀",
+#				"enemy": "seojilee",
+#				"score": [4, 2], // my score가 배열의 시작
+#				"result": "win",
+#		    },
+#		    {
+#			    "date": [2025, 2, 9],
+#				"match_type": "토너먼트",
+#				"enemy": {
+#					    "player1": "seojilee",
+#					    "player2": "taejeong",
+#					    "player3": "hyoengsh",
+#					    "player4": "junhapar",
+#				},
+#				"result": "lose", // win or lose
+#		    },
+#    ],
+#}
 class OneOnOneMatchSerializer(serializers.ModelSerializer):
+    date = serializers.SerializerMethodField()
+    match_type = serializers.SerializerMethodField()
     enemy = serializers.SerializerMethodField()
     score = serializers.SerializerMethodField()
     result = serializers.SerializerMethodField()
@@ -11,20 +51,84 @@ class OneOnOneMatchSerializer(serializers.ModelSerializer):
         model = OneOnOneMatch
         fields = ['date', 'match_type', 'enemy', 'score', 'result']
 
+    # obj -> OneOneOneMatch
+    def get_date(self, obj):
+        date = obj.match_day
+        return [date.year, date.month, date.day]
+
+    def get_match_type(self, obj):
+        return '배틀'
+
     def get_enemy(self, obj):
-        request = self.context.get('request')
-        if not request or not request.user:
-            return None
-        return obj.get_enemy(request.user).username
+        user = self.context.get('user')
+        player1 = obj.player1
+        player2 = obj.player2
+        return player1.username if player1.username != user.username else player2.username
 
     def get_score(self, obj):
-        request = self.context.get('request')
-        if not request or not request.user:
-            return None
-        return obj.get_score(request.user)
+        user = self.context.get('user')
+        return [obj.point1, obj.point2] if obj.player1 == user else [obj.point2, obj.point1]
+
+    # no tie?
+    def get_result(self, obj):
+        user = self.context.get('user')
+        return "win" if (user == obj.player1 and obj.point1 > obj.point2) or (user == obj.player2 and obj.point2 > obj.point1) else "lose"
+
+class TournamentMatchSerializer(serializers.ModelSerializer):
+    date = serializers.SerializerMethodField()
+    match_type = serializers.SerializerMethodField()
+    enemy = serializers.SerializerMethodField()
+    result = serializers.SerializerMethodField()
+
+    class Meta:
+        model = OneOnOneMatch
+        fields = ['date', 'match_type', 'enemy', 'score', 'result']
+
+    # obj -> TournamentMatch
+    def get_date(self, obj):
+        date = obj.match_day
+        return [date.year, date.month, date.day]
+
+    def get_match_type(self, obj):
+        return '토너먼트'
+
+    def get_enemy(self, obj):
+#        user = self.context.get('user')
+       return {
+               "player1": round1_player1,
+               "player2": round1_player2,
+               "player3": round2_player1,
+               "player4": round2_player2
+               }
 
     def get_result(self, obj):
-        request = self.context.get('request')
-        if not request or not request.user:
-            return None
-        return obj.get_result(request.user)
+        user = self.context.get('user')
+        winner = round3_player1 if round3_point1 > round3_point2 else round3_player2
+        if user != winner:
+            return "lose"
+        return "win"
+
+class MatchHistorySerializer(serializers.ModelSerializer):
+    match_history = serializers.SerializerMethodField()
+
+    class Meta:
+        model = CustomUser
+        fields = ['match_history']
+
+    # obj -> CustomUser
+    def get_match_history(self, obj):
+        one_on_one_games = UserOneOnOneGameRecord.objects.filter(user=obj)
+        one_on_one_serialized = OneOnOneMatchSerializer(
+            [record.one_on_one_match_id for record in one_on_one_games],
+            many=True,
+            context={'user': obj}
+            ).data
+
+        tournament_games = UserTournamentGameRecord.objects.filter(user=obj)
+        tournament_serialized = TournamentMatchSerializer(
+            [record.tournament_match_id for record in tournament_games],
+            many=True,
+            context={'user': obj}
+            ).data
+        return one_on_one_serialized + tournament_serialized
+#        return sorted(one_on_one_serialized + tournament_serialized, key=lambda x: x["date"], reverse=True)
--- srcs/backend/srcs/mysite/game_records/views.py
+++ srcs/backend/srcs/mysite/game_records/views.py
@@ -3,57 +3,59 @@ from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from .models import OneOnOneMatch
-from .serializers import OneOnOneMatchSerializer
+from users.models import CustomUser
+from .serializers import MatchHistorySerializer
 from django.db.models import Q
 
 # Create your views here.
-
-# total match history
-# "total_match_history": [
-# {
-#     "match_type": "배틀",
-#     "total_match": 100,
-#     "win": 50,
-#     "lose": 50,
-# }
-# {
-#     "match_type": "토너먼트",
-#     "total_match": 100,
-#     "1st": 25,
-#     "2nd": 25,
-#     "3rd": 25,
-#     "4th": 25,
-# }
-# ],
-# each match history
-# "match_history": [
-#       {
-# 	      "date": [2025, 2, 7],
-# 		    "match_type": "배틀",
-# 		    "enemy": "seojilee",
-# 		    "score": [4, 2], // my score가 배열의 시작
-# 		    "result": "win",
-#       },
-#       {
-# 	      "date": [2025, 2, 9],
-# 		    "match_type": "토너먼트",
-# 		    "rank": {
-# 			    "1st": "seojilee",
-# 			    "2nd": "taejeong",
-# 			    "3rd": "hyoengsh",
-# 			    "4th": "junhapar",
-# 		    }
-# 		    "result": 4,
-#       },
-# ],
-class OneOnOneMatchViewSet(viewsets.ModelViewSet):
-    queryset = OneOnOneMatch.objects.all()
-    serializer_class = OneOnOneMatchSerializer
-
-    # GET: READ
-    @action(detail=False, methods=['get'], url_path='list')
-    def get_matchlist(self, request):
-        user = request.user
-        matches = OneOnOneMatch.objects.filter(Q(player1=user) | Q(player2=user))
-        serializer = self.get_serializer(matches, many=True, context={"request": request})
+class MatchViewSet(viewsets.ViewSet):
+    authentication_classes = []
+    permission_classes = []
+    
+    #/api/games/[username]/
+    def retrieve(self, request, pk=None):
+        try:
+            user = CustomUser.objects.get(username=pk)
+        except CustomUser.DoesNotExist:
+            return Response({"error": "User not found"}, status=400)
+        serializer = MatchHistorySerializer(user)
+        if not serializer.data.get('match_history'):
+            return Response({"error": "No data"}, status=400)
         return Response(serializer.data)
+
+#{
+#    "total_match_history": [
+#	      {
+#		        "match_type": "배틀",
+#		        "total_match": 100,
+#		        "win": 50,
+#		        "lose": 50,
+#	      }
+#	      {
+#		        "match_type": "토너먼트",
+#		        "total_match": 100,
+#		        "win": 50,
+#		        "lose": 50,
+#	      }
+#    ],
+#    "match_history": [
+#		    {
+#		  	    "date": [2025, 2, 7],
+#		        "match_type": "배틀",
+#				    "enemy": "seojilee",
+#				    "score": [4, 2], // my score가 배열의 시작
+#				    "result": "win",
+#		    },
+#		    {
+#			      "date": [2025, 2, 9],
+#				    "match_type": "토너먼트",
+#				    "enemy": {
+#					    "player1": "seojilee",
+#					    "player2": "taejeong",
+#					    "player3": "hyoengsh",
+#					    "player4": "junhapar",
+#				    }
+#				    "result": "lose", // win or lose
+#		    },
+#    ],
+#}
