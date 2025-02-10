895222af2097fb21a42886d59b4897fb706c5852
JeongTJ <dmswl022329@gmail.com>
2025-02-10 14:50:26 +0900

    fix: game record 스키마 변경

--- srcs/backend/srcs/mysite/game_records/models.py
+++ srcs/backend/srcs/mysite/game_records/models.py
@@ -11,13 +11,30 @@ class OneOnOneMatch(models.Model):
 
 class TournamentMatch(models.Model):
     id = models.BigAutoField(primary_key=True)
-    one_on_one_match_id = models.ForeignKey(OneOnOneMatch, null=True, on_delete=models.CASCADE)
-    tournament = models.IntegerField(null=False, blank=False)
-    round = models.IntegerField(null=False, blank=False)
+    round1_player1 = models.ForeignKey(CustomUser, related_name="round1_player1_match", on_delete=models.CASCADE)
+    round1_player2 = models.ForeignKey(CustomUser, related_name="round1_player2_match", on_delete=models.CASCADE)
+    round1_point1 = models.IntegerField(default=0, null=False, blank=False)
+    round1_point2 = models.IntegerField(default=0, null=False, blank=False)
+    round2_player1 = models.ForeignKey(CustomUser, related_name="round2_player1_match", on_delete=models.CASCADE)
+    round2_player2 = models.ForeignKey(CustomUser, related_name="round2_player2_match", on_delete=models.CASCADE)
+    round2_point1 = models.IntegerField(default=0, null=False, blank=False)
+    round2_point2 = models.IntegerField(default=0, null=False, blank=False)
+    round3_player1 = models.ForeignKey(CustomUser, related_name="round3_player1_match", on_delete=models.CASCADE)
+    round3_player2 = models.ForeignKey(CustomUser, related_name="round3_player2_match", on_delete=models.CASCADE)
+    round3_point1 = models.IntegerField(default=0, null=False, blank=False)
+    round3_point2 = models.IntegerField(default=0, null=False, blank=False)
+    round4_player1 = models.ForeignKey(CustomUser, related_name="round4_player1_match", on_delete=models.CASCADE)
+    round4_player2 = models.ForeignKey(CustomUser, related_name="round4_player2_match", on_delete=models.CASCADE)
+    round4_point1 = models.IntegerField(default=0, null=False, blank=False)
+    round4_point2 = models.IntegerField(default=0, null=False, blank=False)
     match_day = models.DateTimeField(auto_now_add=True)
 
-class UserGameRecord(models.Model):
+class UserOneOnOneGameRecord(models.Model):
     id = models.BigAutoField(primary_key=True)
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    one_on_one_match_id = models.ForeignKey(OneOnOneMatch, null=True, on_delete=models.CASCADE)
-    tournament_match_id = models.ForeignKey(TournamentMatch, null=True, on_delete=models.CASCADE)
+    one_on_one_match_id = models.ForeignKey(OneOnOneMatch, null=False, on_delete=models.CASCADE)
+    
+class UserTournamentGameRecord(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    tournament_match_id = models.ForeignKey(TournamentMatch, null=False, on_delete=models.CASCADE)
