532448ce31847728f460ba6332ef48e7ea2162a6
JeongTJ <dmswl022329@gmail.com>
2025-02-12 14:15:25 +0900

    feat: websocket game consumer

--- srcs/backend/srcs/mysite/chat/consumers.py
+++ srcs/backend/srcs/mysite/chat/consumers.py
@@ -2,7 +2,10 @@
 import json
 
 from channels.generic.websocket import AsyncWebsocketConsumer
+from django.contrib.auth.models import AnonymousUser
 
+import logging
+logger = logging.getLogger('chat') 
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -34,3 +37,37 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         # Send message to WebSocket
         await self.send(text_data=json.dumps({"message": message}))
+
+class GameBattleConsumer(AsyncWebsocketConsumer):
+    active_users = {}
+    async def connect(self):
+        self.user = self.scope["user"]
+        self.username = None
+        
+        logger.debug("connect test")
+        try:
+            if self.user is None or isinstance(self.user, AnonymousUser):
+                logger.debug("=" * 10)
+                logger.debug("User not authenticated")
+                logger.debug("=" * 10)
+                raise Exception('User not authenticated')
+            self.username = self.user.username
+            if self.active_users[self.username] == True:
+                raise Exception('multi client error')
+            self.active_users[self.username] = True
+            await self.accept()
+        except Exception as e:
+            logger.error(str(e))
+            await self.close(code=4001)
+
+    async def disconnect(self, close_code):
+        if not self.username is None:
+            del self.active_users[self.username]
+
+    # Receive message from WebSocket
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+            
+        if data.get("message"):
+            message = f"{self.username}: {data.get("message")}"
+            await self.send(text_data=json.dumps({"message": message}))
