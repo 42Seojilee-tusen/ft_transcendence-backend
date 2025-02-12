4f623a3274e72eb75121bb447a201baa1cd02ad7
JeongTJ <dmswl022329@gmail.com>
2025-02-12 16:14:48 +0900

    feat: matching system make

--- srcs/backend/srcs/mysite/chat/consumers.py
+++ srcs/backend/srcs/mysite/chat/consumers.py
@@ -3,6 +3,7 @@ import json
 
 from channels.generic.websocket import AsyncWebsocketConsumer
 from django.contrib.auth.models import AnonymousUser
+import uuid
 
 import logging
 logger = logging.getLogger('chat') 
@@ -40,34 +41,64 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
 class GameBattleConsumer(AsyncWebsocketConsumer):
     active_users = {}
+    waiting_users = []
+    
     async def connect(self):
         self.user = self.scope["user"]
-        self.username = None
-        
-        logger.debug("connect test")
+        self.group_name = None
         try:
             if self.user is None or isinstance(self.user, AnonymousUser):
-                logger.debug("=" * 10)
-                logger.debug("User not authenticated")
-                logger.debug("=" * 10)
+                logger.error("User not authenticated")
                 raise Exception('User not authenticated')
-            self.username = self.user.username
-            if self.active_users[self.username] == True:
-                raise Exception('multi client error')
-            self.active_users[self.username] = True
+            self.active_users[self.channel_name] = self.active_users.get(self.channel_name, 0) + 1
+            # if self.channel_name in self.active_users and self.active_users[self.channel_name] > 1:
+            #     logger.error(f"{self.user.username} is multi client error")
+            #     raise Exception('multi client error')
             await self.accept()
+            self.waiting_users.append({'channel_name': self.channel_name, 'user': self.user})
+            if len(self.waiting_users) >= 2:
+                user1 = self.waiting_users.pop(0)
+                user2 = self.waiting_users.pop(0)
+                self.group_name = f"group_{uuid.uuid4().hex}"
+                await self.channel_layer.group_add(self.group_name, user1['channel_name'])
+                await self.channel_layer.group_add(self.group_name, user2['channel_name'])
+                await self.channel_layer.group_send(
+                    self.group_name, {'type':'matching.on', "message": "매칭이 잡혔습니다", "group_name": self.group_name}
+                )
         except Exception as e:
             logger.error(str(e))
             await self.close(code=4001)
+            return 
 
     async def disconnect(self, close_code):
-        if not self.username is None:
-            del self.active_users[self.username]
+        if self.channel_name and self.channel_name in self.active_users:
+            self.active_users[self.channel_name] -= 1
+            if self.group_name:
+                await self.channel_layer.group_send(
+                    self.group_name, {'type':'matching.off', "message": "매칭이 끊겼습니다"}
+                )
+                await self.channel_layer.group_discard(self.group_name, self.channel_name)
+                self.group_name = None
+            for i, user in enumerate(self.waiting_users):
+                if user['channel_name'] == self.channel_name:
+                    del self.waiting_users[i]
+                    break  # 일치하는 유저를 찾았으면 반복 종료
+            if self.active_users[self.channel_name] <= 0:
+                self.active_users.pop(self.channel_name, None)
+        logger.debug("close_code: " + str(close_code))
 
     # Receive message from WebSocket
     async def receive(self, text_data):
         data = json.loads(text_data)
-            
         if data.get("message"):
             message = f"{self.username}: {data.get("message")}"
             await self.send(text_data=json.dumps({"message": message}))
+
+    async def matching_on(self, event):
+        message = event['message']
+        self.group_name = event['group_name']
+        await self.send(text_data=json.dumps({"message": message}))
+
+    async def matching_off(self, event):
+        message = event['message']
+        await self.send(text_data=json.dumps({"message": message}))
