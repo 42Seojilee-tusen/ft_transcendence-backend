60df5f67483e5468e23178c9372c0d79e1860d45
JeongTJ <dmswl022329@gmail.com>
2025-02-22 16:28:09 +0900

    feat: 4인용 게임 추가 예정

--- srcs/backend/srcs/mysite/chat/matchmanager.py
+++ srcs/backend/srcs/mysite/chat/matchmanager.py
@@ -23,13 +23,34 @@ class MatchManager():
     # 매칭시켜주는 로직
     # 매칭될 유저가 없으면 대기열에 추가 후 None 반환
     # 있으면 GameGroup객체 반환
-    def matching(self, channel2, user2):
+    def matching2(self, channel2, user2):
         # 매칭 로직
-        if len(self.waiting_channels) <= 0:
+        if len(self.waiting_channels) < 1:
             self.add_waiting(channel2, user2)
             return None
-        user1 = self.waiting_channels.pop(0)
-        channel1 = user1['channel_name']
-        user1 = user1['user']
-        game_group = GameGroup(channel1, channel2, user1, user2)
+        waiting_channel1 = self.waiting_channels.pop(0)
+        channel1 = waiting_channel1['channel_name']
+        user1 = waiting_channel1['user']
+        channels = [channel1, channel2]
+        users = [user1, user2]
+        game_group = GameGroup(channels, users)
+        return game_group
+
+    def matching4(self, channel4, user4):
+        # 매칭 로직
+        if len(self.waiting_channels) < 3:
+            self.add_waiting(channel4, user4)
+            return None
+        waiting_channel1 = self.waiting_channels.pop(0)
+        channel1 = waiting_channel1['channel_name']
+        user1 = waiting_channel1['user']
+        waiting_channel2 = self.waiting_channels.pop(0)
+        channel2 = waiting_channel2['channel_name']
+        user2 = waiting_channel2['user']
+        waiting_channel3 = self.waiting_channels.pop(0)
+        channel3 = waiting_channel3['channel_name']
+        user3 = waiting_channel3['user']
+        channels = [channel1, channel2, channel3, channel4]
+        users = [user1, user2, user3, user4]
+        game_group = GameGroup(channels, users)
         return game_group
