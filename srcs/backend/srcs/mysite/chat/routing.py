483b91e34267331d1f8fd4a7f132f03d67d92f49
JeongTJ <dmswl022329@gmail.com>
2025-03-02 14:17:24 +0900

    fix: routing

--- srcs/backend/srcs/mysite/chat/routing.py
+++ srcs/backend/srcs/mysite/chat/routing.py
@@ -2,9 +2,10 @@
 from django.urls import re_path
 
 from . import consumers
+from . import tournamentconsumer
 
 websocket_urlpatterns = [
     # re_path(r"ws/chat/(?P<room_name>\w+)/$", consumers.ChatConsumer.as_asgi()),
     re_path("ws/game/battle/", consumers.GameBattleConsumer.as_asgi()),
-    re_path("ws/game/tournament", consumers.GameTournamentConsumer.as_asgi()),
+    re_path("ws/game/tournament", tournamentconsumer.GameTournamentConsumer.as_asgi()),
 ]
