300231a2c0715e9008d3133cc91c89d74b42aab3
JeongTJ <dmswl022329@gmail.com>
2025-03-03 15:46:37 +0900

    feat: 온 오프라인 상태 전용 앱 생성

--- srcs/backend/srcs/mysite/config/asgi.py
+++ srcs/backend/srcs/mysite/config/asgi.py
@@ -19,7 +19,10 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from utils.channelsmiddleware import JWTAuthMiddlewareStack
 
-from chat.routing import websocket_urlpatterns
+from chat.routing import websocket_urlpatterns as game_websocket
+from online_status.routing import websocket_urlpatterns as online_websocket
+
+combine_websocker_urlpatterns = game_websocket + online_websocket
 
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
@@ -28,7 +31,7 @@ application = ProtocolTypeRouter({
     "http": django_asgi_app,
 	"websocket": AllowedHostsOriginValidator(
 		# AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
-		JWTAuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+		JWTAuthMiddlewareStack(URLRouter(combine_websocker_urlpatterns))
 	),
     # Just HTTP for now. (We can add other protocols later.)
 })
