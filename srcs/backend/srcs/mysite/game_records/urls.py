1af276509fee3de38c1e56afda420d9b34a4e399
leeluna0476 <leeluna0476@daum.net>
2025-03-08 15:06:10 +0900

    refactor: change viewset to apiview for a single view

--- srcs/backend/srcs/mysite/game_records/urls.py
+++ srcs/backend/srcs/mysite/game_records/urls.py
@@ -2,10 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter, SimpleRouter
 from . import views
 
-router = DefaultRouter()
-router.register(r'me', views.MatchAuthViewSet, basename='my-game-records')
-router.register(r'', views.MatchViewSet, basename='game-records')
-
 urlpatterns = [
-    path('', include(router.urls)),
+    path('me/', views.MatchAuthView.as_view()),
+    path('<str:username>/', views.MatchView.as_view()),
 ]
--- srcs/backend/srcs/mysite/game_records/views.py
+++ srcs/backend/srcs/mysite/game_records/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework.views import APIView
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from .models import OneOnOneMatch
@@ -7,20 +7,18 @@ from users.models import CustomUser
 from .serializers import MatchHistorySerializer
 
 # Create your views here.
-class MatchAuthViewSet(viewsets.ViewSet):
-    #/api/games/me/
-    def list(self, request):
+class MatchAuthView(APIView):
+    def get(self, request):
         user = request.user
         serializer = MatchHistorySerializer(user)
         if not serializer.data.get('match_history'):
             return Response({"error": "Data not found"}, status=400)
         return Response(serializer.data)
 
-class MatchViewSet(viewsets.ViewSet):
-    #/api/games/[username]/
-    def retrieve(self, request, pk=None):
+class MatchView(APIView):
+    def get(self, request, username=None):
         try:
-            user = CustomUser.objects.get(username=pk)
+            user = CustomUser.objects.get(username=username)
         except CustomUser.DoesNotExist:
             return Response({"error": "User not found"}, status=400)
         serializer = MatchHistorySerializer(user)
