61c2c8a44645e91e4a5135dd98463e9e1df640af
JeongTJ <dmswl022329@gmail.com>
2025-03-08 17:06:50 +0900

    fix: 게임 나갈 떄 유저 체크 버그

--- srcs/backend/srcs/mysite/chat/tournamentgamegroup.py
+++ srcs/backend/srcs/mysite/chat/tournamentgamegroup.py
@@ -251,7 +251,7 @@ class TournamentGameGroup:
         logger.debug("게임 끝")
         if self.online_channels[channels[0]] == False:
             await self.append_game_result([-1,5])
-        elif self.online_channels[channels[0]] == False:
+        elif self.online_channels[channels[1]] == False:
             await self.append_game_result([5,-1])
         else:
             await self.append_game_result()
