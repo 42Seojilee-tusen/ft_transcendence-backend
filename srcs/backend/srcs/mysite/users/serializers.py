bddabc34cf3d2512229667dc18edfc5d0f4c4282
leeluna0476 <leeluna0476@daum.net>
2025-03-08 17:23:17 +0900

    fix: do not serialize id for CustomUser. is not needed

--- srcs/backend/srcs/mysite/users/serializers.py
+++ srcs/backend/srcs/mysite/users/serializers.py
@@ -6,7 +6,7 @@ import re, os
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['id', 'username', 'email', 'profile_image', 'is_online']  # 필요한 필드만 선택
+        fields = ['username', 'email', 'profile_image', 'is_online']  # 필요한 필드만 선택
         read_only_fields = ['is_online']
 
     def validate(self, data):
